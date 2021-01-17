77f3bffae578dbe1d3ee8159ebb1dfd5eb48d1f2
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-01-17 17:01:10 -0300

    correção port production

--- src/config/server.js
+++ src/config/server.js
@@ -7,19 +7,32 @@ const cors = require('cors')
 const queryParser = require('express-query-int')
 
 // Set default node environment to development 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+//process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(cors())
 server.use(queryParser())
 
+
 // Vai carregar o express se for o modo de desenvolvimento
 // se for TESTE não irá carregar
-if (server.get('env') == 'development') {
-    server.listen(port, function() {
-        console.log(`Backend is running on port ${port} at ${server.get('env')}.`)
-    })
-}
+// if (server.get('env') == 'development') {
+//     server.listen(port, function() {
+//         console.log(`Backend is running on port ${port} at ${server.get('env')}.`)
+//     })
+// }
+
+
+server.listen(port, function() {
+    console.log(`Backend is running on port ${port} at ${server.get('env')}.`)
+})
+
+
+console.log('vv server start vv')
+console.log('Server is listening on port ' + server.port);
+console.log(server.get('env'))
+console.log('^^ server start ^^')
+
 
 module.exports = server
