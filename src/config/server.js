55bba78643a44517150d553b08379a8ef272409d
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-01-17 17:41:13 -0300

    Mais acertos de porta

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
-const port = 3003
+const port = 80
 
 const bodyParser = require('body-parser')
 const express = require('express')
@@ -6,33 +6,17 @@ const server = express()
 const cors = require('cors')
 const queryParser = require('express-query-int')
 
-// Set default node environment to development 
-//process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(cors())
 server.use(queryParser())
 
-
-// Vai carregar o express se for o modo de desenvolvimento
-// se for TESTE não irá carregar
-// if (server.get('env') == 'development') {
-//     server.listen(port, function() {
-//         console.log(`Backend is running on port ${port} at ${server.get('env')}.`)
-//     })
-// }
-
-
-server.listen(port, function() {
+server.listen(process.env.PORT || port, function() {
     console.log(`Backend is running on port ${port} at ${server.get('env')}.`)
+    console.log('vv server start vv')
+    console.log('Server is listening on port ' + process.env.PORT);
+    console.log(server.get('env'))
+    console.log('^^ server start ^^')
 })
-
-
-console.log('vv server start vv')
-console.log('Server is listening on port ' + server.port);
-console.log(server.get('env'))
-console.log('^^ server start ^^')
-
 
 module.exports = server
