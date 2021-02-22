b394b13593c1a4a5c80e5af31ad7c32d67217b69
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-02-21 22:17:57 -0300

    Correção do cronjob

--- src/config/cronjob.js
+++ src/config/cronjob.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule')
 
-const job = schedule.scheduleJob('*/15 * * * * *', function(){
+const job = schedule.scheduleJob('* * * 1 * *', function(){
     console.log('The answer to life, the universe, and everything!');
   });
 
