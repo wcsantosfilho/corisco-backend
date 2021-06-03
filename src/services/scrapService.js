0f16718a6d4c50837fda47c02f661c31df1c91eb
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-06-02 21:57:15 -0300

    Tratamento dos dados lidos no scrap

--- src/services/scrapService.js
+++ src/services/scrapService.js
@@ -1,4 +1,6 @@
 const cheerio = require('cheerio');
+const { data } = require('cheerio/lib/api/attributes');
+const { isNumber } = require('lodash');
 const puppeteer = require('puppeteer');
 
 /* scrapService
@@ -17,15 +19,10 @@ const url = 'http://www.loterias.caixa.gov.br/wps/portal/loterias/landing/megase
     // last bet is equal last draw?
     async scrapLastDraw() {
         try {
-            console.log('launching browser...')
             const browser = await puppeteer.launch()
-            console.log('new page...')
             const page = await browser.newPage()
-            console.log('go to...' + url)
             await page.goto(url)
-            console.log('waitForSelector...')
             await page.waitForSelector('#conteudoresultado')
-            console.log('title...' + page._target._targetInfo.title)
             let agaDois = await page.evaluate( () => {
                 let results = []
                 let items = document.querySelectorAll('h2')
@@ -37,15 +34,32 @@ const url = 'http://www.loterias.caixa.gov.br/wps/portal/loterias/landing/megase
                 return results
             })
             await browser.close()
-            var drawText = agaDois.filter( (item) => {
+            var drawNumberInPage = agaDois.filter( (item) => {
                 // Look for "Resultado Concurso"
-                let pattern = /Resultado Concurso/g;
-                return pattern.test(item.text);
+                let pattern = /Resultado Concurso/g
+                return pattern.test(item.text)
             })
-            console.log('XPTO: ', drawText)
+            let pattern = /Resultado Concurso (\d+) \((\d{1,2})\/(\d{1,2})\/(\d{2,4})\)/g
+            var dataExtracted = pattern.exec(drawNumberInPage[0].text)
+            if (dataExtracted == null ) {
+                throw "No draw read at all"
+            }
+            let lastDraw = isNaN(parseInt(dataExtracted[1])) ? null : parseInt(dataExtracted[1])
+            let dayLastDraw = isNaN(parseInt(dataExtracted[2])) ? null : parseInt(dataExtracted[2])
+            let monthLastDraw = isNaN(parseInt(dataExtracted[3])) ? null : parseInt(dataExtracted[3])
+            let yearLastDraw = isNaN(parseInt(dataExtracted[4])) ? null : parseInt(dataExtracted[4])
+            let textInCaixaWebPage = dataExtracted[0]
+            if (lastDraw == null || dayLastDraw == null || monthLastDraw == null || yearLastDraw == null) {
+                throw "Problem in scrap"
+            }
 
             return { status: 200,
-                payload: drawText 
+                payload: { status: 200,
+                    lastDraw: lastDraw,
+                    dayLastDraw: dayLastDraw,
+                    monthLastDraw: monthLastDraw,
+                    yearLastDraw: yearLastDraw,
+                    textInCaixaWebPage: textInCaixaWebPage }
             }
         } catch (e) {
             console.log('Erro no catch: ' + e)
