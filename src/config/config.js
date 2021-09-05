c99dca2c5e57cf4d5b9eaaf5723c75e30c63be14
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-09-05 16:52:34 -0300

    correção de dados

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,6 @@ config.development = {};
 
 config.production.mongodbURI = process.env.MONGODB_URI
 config.test.mongodbURI = "mongodb://localhost/coriscoTST"
-//config.development.mongodbURI = "mongodb://localhost/corisco"
-config.development.mongodbURI = "mongodb+srv://coriscoDbUser:66JywPUeIvPGUGGM@corisco.vq9ow.mongodb.net/corisco?retryWrites=true&w=majority"
+config.development.mongodbURI = "mongodb://localhost/corisco"
 
 module.exports = config;
