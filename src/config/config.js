c3f176c181f22779faed1fe66455a16e59ee056d
Walter Santos Filho <wcsantosfilho@gmail.com>
2021-09-05 16:49:09 -0300

    Merge da branch scrap

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,7 @@ config.development = {};
 
 config.production.mongodbURI = process.env.MONGODB_URI
 config.test.mongodbURI = "mongodb://localhost/coriscoTST"
-config.development.mongodbURI = "mongodb://localhost/corisco"
+//config.development.mongodbURI = "mongodb://localhost/corisco"
+config.development.mongodbURI = "mongodb+srv://coriscoDbUser:66JywPUeIvPGUGGM@corisco.vq9ow.mongodb.net/corisco?retryWrites=true&w=majority"
 
 module.exports = config;
